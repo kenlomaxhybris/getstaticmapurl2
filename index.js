887257ba20228075cd83a058ec66601d764e9ce9
Ken Lomax <ken.lomax@sap.com>
2019-05-20 14:14:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var https = require('https');
+var util = require('util')
+
+initjason = (jason) => {
+  // when called via "functions call", the jason.daa is found in body..
+  if (jason && jason.body)
+    jason = jason.body
+  // When called via events (see later), jason is in jason.data..
+  if (jason && jason.data)
+    jason = jason.data;
+  return jason
+}
+  
+exports.getstaticmapurl2 = (jason, res) => {
+  jason = initjason(jason)
+  var END_POINT= "https://ns20190515a.cluster20190508d.cluster.extend.cx.cloud.sap/purchasesByNameSpace?nameSpace=mylambda";
+
+  var request = https.get(END_POINT, (res1) => {	
+	res1.on('data', (d) => {
+		var purchases =	JSON.parse(d)
+		var purchasesurl=""
+		var center=""
+		purchases.forEach( purchase => {
+	        const address = purchase["postalCode"]+","+purchase["town"]
+	        purchasesurl += purchase["town"]+","+ purchase["postalCode"] +"|"
+	        if (center=="")
+	        	center = purchase["town"]+","+ purchase["postalCode"]   	
+	    });
+
+		purchasesurl =  purchasesurl.slice(0, purchasesurl.length-1);
+	    console.log("purchasesurl " + purchasesurl)
+	    console.log("center " + center)
+	    purchasesurl = encodeURIComponent(purchasesurl)
+	    center = encodeURIComponent(center);
+	    console.log("purchasesurl" +purchasesurl)
+	 	console.log("center" +center)
+
+       res.setHeader('Content-Type', 'application/json');
+    	res.set('Access-Control-Allow-Origin', '*')    
+	   	.set('Access-Control-Allow-Methods', 'GET, POST')
+      
+		var staticmapurl = "https://maps.googleapis.com/maps/api/staticmap?center="+center+"&zoom=1&size=400x400&markers=color:blue%7Clabel:S%7C"+purchasesurl+"&markers=size:tiny%7Ccolor:green%7CDelta+Junction,AK&markers=size:mid%7Ccolor:0xFFFF00%7Clabel:C%7CTok,AK&key=AIzaSyBpjrPXAwcNXinDaRCy3ObPHlBHuAGhwR4"
+		console.log("staticmapurl" +staticmapurl)
+		if (res){
+			res.status(200).send('{"url":"'+staticmapurl +'"}');	
+		}	
+	})
+  })
+}
+
+
